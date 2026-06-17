5dea56db77f3ed15d74cf050e65881550170542a
Vikraam <vikraam17@gmail.com>
2026-06-17 09:51:24 +0530

    Effect added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Parent from './component/Parent'
 import State from './component/State'
 import Navbar from './component/Navbar'
 import {Routes,Route} from 'react-router-dom'
+import Effect from './component/Effect'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Parent/>}></Route>
         <Route path='/useState' element={<State/>}/>
+        <Route path='/useEffect' element={<Effect/>}/>
       </Routes>
     </>
   )
